231e126f7b7bb22e8f6133e1aed58f9b453a3e6a
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2023-09-28 23:08:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //your JS code here. If required
 document.addEventListner("DOMContentLoaded",() =>{
-	document.write("DOM load success");
-})
+	// document.write("DOM load success");
+	document.body.innerText="DOM load success";
+});
